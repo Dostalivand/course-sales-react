4f3621202f02603f969d62592134abceee403241
Hosein-DAV <hoseindost33@gmail.com>
2025-03-19 00:13:34 +0330

    update

--- src/Components/DescriptionCourseBox/DescriptionCourseBox.jsx
+++ src/Components/DescriptionCourseBox/DescriptionCourseBox.jsx
@@ -140,8 +140,8 @@ export default function DescriptionCourseBox({ courseTitle, teacherPhoto, teache
                                 </div>
                                 <div className='mb-4'>
                                     <h2 id="accordion-open-heading-2">
-                                        <button type="button" className="flex items-center justify-between w-full p-5 font-medium text-gray-700 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-100 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3 cursor-pointer" data-accordion-target="#accordion-open-body-2" aria-expanded="false" aria-controls="accordion-open-body-2">
-                                            <span className="flex items-center"><span className='text-base text-orange-400 Dana-Bold me-1'>فصل دوم : </span> مباحث نیمه حرفه ای و متوسط</span>
+                                        <button type="button" className="flex items-center justify-between w-full p-5 font-medium text-gray-700 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-100 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3 cursor-pointer" data-accordion-target="#accordion-open-body-1" aria-expanded="true" aria-controls="accordion-open-body-1">
+                                            <span className="flex items-center"><span className='text-base text-orange-400 Dana-Bold me-1'>فصل دوم : </span>مباحث متوسطه و آنالیز داده</span>
                                             <svg data-accordion-icon className="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5 5 1 1 5" />
                                             </svg>
